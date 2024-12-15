724ea3198a960e7f10e782a052119885cb18d236
Manoj Biswal <manojbiswal521@gmail.com>
2024-12-15 15:56:19 +0530

    diff kind of route path

--- src/app.js
+++ src/app.js
@@ -2,26 +2,25 @@ const express = require("express");
 
 const app = express(); //this indicate the express js application
 
-/**
- * if you add this on ist line it will execute only this  
- * app.use("/user", (req, res) => {
-  res.send("this is from user");
-}); */
-
-//this will only handle get calls to /user
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Manoj", lastName: "Biswal" });
-});
-app.post("/user", (req, res) => {
-  //   console.log("save the data to the data base");
-  res.send("save the data to the data base");
-});
-app.delete("/user", (req, res) => {
-  res.send("delete the data from the data base");
-});
-//this will only match all the http method Api calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello i am from the test server...");
+//
+// app.get("/user", (req, res) => {
+//   res.send({ firstName: "Manoj", LastName: "Biswal" });
+// });
+// below b is optional
+// app.get("/ab?c", (req, res) => {
+//   res.send({ firstName: "Manoj", LastName: "Biswal" });
+// });
+// below you can add the extra b this get mode called with out any error
+// app.get("/ab+c", (req, res) => {
+//   res.send({ firstName: "Manoj", LastName: "Biswal" });
+// });
+// below you can add any thing after the * burt here you must start with ab
+// app.get("/ab*c", (req, res) => {
+//   res.send({ firstName: "Manoj", LastName: "Biswal" });
+// });
+// below the bc are optional
+app.get("/a(bc)?d", (req, res) => {
+  res.send({ firstName: "Manoj", LastName: "Biswal" });
 });
 app.listen(8080, () => {
   console.log("server is successfully listening on the port 8080!!!");
