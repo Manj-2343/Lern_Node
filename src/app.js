8846fad002825068ec662e8a246b5753920dc5a3
Manoj Biswal <manojbiswal521@gmail.com>
2024-12-15 16:11:46 +0530

    add req.query and req,params

--- src/app.js
+++ src/app.js
@@ -2,25 +2,10 @@ const express = require("express");
 
 const app = express(); //this indicate the express js application
 
-//
-// app.get("/user", (req, res) => {
-//   res.send({ firstName: "Manoj", LastName: "Biswal" });
-// });
-// below b is optional
-// app.get("/ab?c", (req, res) => {
-//   res.send({ firstName: "Manoj", LastName: "Biswal" });
-// });
-// below you can add the extra b this get mode called with out any error
-// app.get("/ab+c", (req, res) => {
-//   res.send({ firstName: "Manoj", LastName: "Biswal" });
-// });
-// below you can add any thing after the * burt here you must start with ab
-// app.get("/ab*c", (req, res) => {
-//   res.send({ firstName: "Manoj", LastName: "Biswal" });
-// });
-// below the bc are optional
-app.get("/a(bc)?d", (req, res) => {
-  res.send({ firstName: "Manoj", LastName: "Biswal" });
+app.get("/user/:userId", (req, res) => {
+  console.log(req.query); //to get the query param
+  console.log(req.params); //to get the parama;
+  res.send({ firstName: "Manoj", lastName: "Biswal" });
 });
 app.listen(8080, () => {
   console.log("server is successfully listening on the port 8080!!!");
