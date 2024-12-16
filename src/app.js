e73c072dd5b33915f1f520e417ae840bd5be2e66
Manoj Biswal <manojbiswal521@gmail.com>
2024-12-16 16:19:57 +0530

    Multiple_route_handlers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,18 @@ const express = require("express");
 
 const app = express(); //this indicate the express js application
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.query); //to get the query param
-  console.log(req.params); //to get the parama;
-  res.send({ firstName: "Manoj", lastName: "Biswal" });
-});
+//syntax:app.use("/route",[rh1,rh2,rh3]) or app.use("/route",[rh1,rh2],rh3) or app.use("/route",rh1,rh2)
+app.use("/user", [
+  (req, res, next) => {
+    console.log("this is 1st response");
+    next(); //to handle the multiple  route handler
+  },
+  (req, res) => {
+    console.log("this is second response");
+    res.send("route handler two ");
+  },
+]); //the call back function is known as the route handler
+
 app.listen(8080, () => {
   console.log("server is successfully listening on the port 8080!!!");
 });
-
-//order of the route is matters
-// htpp method: get post put delete
