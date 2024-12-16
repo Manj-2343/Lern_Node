b0ee098d0cfb26447c1b40778373553b621a8613
Manoj Biswal <manojbiswal521@gmail.com>
2024-12-17 03:50:18 +0530

    connect to the bd by using code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const connectDb = require("./config/db.js");
+const User = require("./models/user.js");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Virat",
+    lastName: "Kohli",
+    email: "virat@gmail.com",
+    password: "12345",
+  };
+  // creating the new instance of teh userModel
+  const user = new User(userObj);
+  await user.save();
+  res.send("User added successfully");
+});
+// __v:0 this is  the version of you document
+
 connectDb()
   .then(() => {
     console.log("Database connection established......");
